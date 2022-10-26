302064e3132129825dd6899336ff37b2b4e0d0ea
NOOJEY <85036337+NOOJEY@users.noreply.github.com>
2022-10-26 14:50:22 +0900

    Add files via upload

--- PetFeeder_ArduinoSource/PetFeeder/PetFeeder.ino
+++ PetFeeder_ArduinoSource/PetFeeder/PetFeeder.ino
@@ -1,4 +1,3 @@
-
 #include <DS1302.h>
 #include <DFRobotDFPlayerMini.h>
 #include <SoftwareSerial.h>
@@ -8,16 +7,18 @@
 #define calibration_factor -7050.0
 #define DOUT 3
 #define CL 2
+#define IR_DETECT 29
 HX711 scale(DOUT, CL);
+DFRobotDFPlayerMini myDFPlayer;
 SoftwareSerial BTSerial(10, 11);
-/*블루투스 10,11 
-  mp3 rx1/tx1 18 19   
+/*블루투스 10,11
+  mp3 rx1/tx1 18 19
   무게 2, 3
   모터 4, 5, 6, 7
   RTC: SCL 23 I/O 25 RST 27
 */
-bool commandActive = false;
-bool secommandActive = false;
+bool amountActive = false;
+bool timeSetActive = false;
 bool remainCheckActive = false;
 float tempWeight = 0;
 const int stepsPerRevolution = 64;
@@ -30,12 +31,10 @@ Time t;
 
 String temp = "";
 
-int inputCount = 0;
-int ftCount = 0;
-int feedAmount = 100; //default 값
+int feedAmount = 100; //default Amount
 int feedCNT = 0;
 
-int MorningFeedH = 8; // 사료 급여시간 설정
+int MorningFeedH = 8; // default Feeding time
 int MorningFeedM = 0;
 int LunchFeedH = 12;
 int LunchFeedM = 00;
@@ -47,10 +46,11 @@ float remainFeed = 0;
 
 byte data = "";
 float feedData[10] = {};
-String byteToString ="";
-DFRobotDFPlayerMini myDFPlayer;
+String byteToString = "";
 
-void setup(){
+int ledPin = 31;
+
+void setup() {
   Serial1.begin(9600);
   BTSerial.begin(9600);
   Serial.begin(9600);
@@ -62,170 +62,189 @@ void setup(){
   scale.begin(DOUT, CL);
   scale.tare();
   scale.set_scale();
-  myrtc.halt(false);                 
+  myrtc.halt(false);
   myrtc.writeProtect(false);
   myStepper.setSpeed(500);
 
-  }
+  pinMode(IR_DETECT, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
 
 
-void loop(){
+void loop() {
   scale.set_scale(calibration_factor);
   t = myrtc.getTime();
-
-
-  /*블루투스로 사료 잔여량 확인*/
-  while(BTSerial.available()){
+  int IR = digitalRead(IR_DETECT);
+  
+  if (IR == 0) digitalWrite(ledPin, HIGH);
+  if (IR == 1) digitalWrite(ledPin, LOW);
+  while (BTSerial.available()) {
     data = BTSerial.read();
-    if(data == 'A'){
+
+    //Amount Slicing
+    if (data == 'A') {
       byteToString = "";
     }
-    if(data == 'T'){
+    //Time Slicing
+    if (data == 'T') {
       byteToString = "";
     }
+
     byteToString += char(data);
     Serial.println(byteToString);
 
-    // seekBar로 급여량 설정하는 부분
-    if(byteToString == "Amount"){
+    // Ready for set feedAmount with seekBar
+    if (byteToString == "Amount") {
       Serial.println(byteToString);
-      commandActive = true;
+      amountActive = true;
       byteToString = "";
       continue;
-    } 
-    // EditText로 급여 시간 설정하는 부분
-    if(byteToString == "Time"){
+    }
+    // Ready for set feeding time with EditText
+    if (byteToString == "Time") {
       Serial.println(byteToString);
-      secommandActive = true;
+      timeSetActive = true;
       byteToString = "";
       continue;
     }
 
-    if(byteToString == "playtwo"){
+    //manual feeding
+    if (byteToString == "playtwo") {
       byteToString = "";
       feeding();
     }
-    else if(byteToString == "feedDataRequest"){
+
+    // feedData sort, then send to android
+    else if (byteToString == "feedDataRequest") {
       byteToString = "";
-      /*정렬 후 전송*/
-      if(feedCNT > 8){
-        for(int i = 0; i < 9; i++){
+      if (feedCNT > 8) {
+        for (int i = 0; i < 9; i++) {
           BTSerial.print(feedData[i]);
           delay(100);
         }
       }
 
-      else if(feedCNT <= 8){
-        for(int i = 0 ; i < (feedCNT % 9); i++){
-          BTSerial.print(feedData[i]);
-          delay(100);
+      else if (feedCNT <= 8) {
+
+        for (int i = 0 ; i < feedCNT; i++) {
+          if (feedCNT < 1) {
+            BTSerial.print("저장된 데이터가 없습니다");
+          }
+          else {
+            BTSerial.print(feedData[i]);
+            delay(100);
+          }
         }
       }
     }
   }
   delay(100);/*오동작 방지 딜레이*/
 
-  if(commandActive){
+  if (amountActive) {
     feedAmount = byteToString.toInt();
-    commandActive = false;
+    amountActive = false;
     byteToString = "";
   }
-  if(secommandActive){
-    for(int i=0; i<12; i++){
-      if(i<2){
+
+  if (timeSetActive) {
+    for (int i = 0; i < 12; i++) {
+      if (i < 2) {
         temp += byteToString[i];
-        if(i==1){
+        if (i == 1) {
           MorningFeedH = temp.toInt();
           temp = "";
         }
       }
-      if(i>=2 && i<4){
+      if (i >= 2 && i < 4) {
         temp += byteToString[i];
-        if(i==3){
+        if (i == 3) {
           MorningFeedM = temp.toInt();
 
           temp = "";
         }
       }
-      if(i>=4 && i<6){
+      if (i >= 4 && i < 6) {
         temp += byteToString[i];
-        if(i==5){
+        if (i == 5) {
           LunchFeedH = temp.toInt();
           temp = "";
         }
       }
-      if(i>=6 && i<8){
+      if (i >= 6 && i < 8) {
         temp += byteToString[i];
-        if(i==7){
+        if (i == 7) {
           LunchFeedM = temp.toInt();
           temp = "";
         }
       }
-      if(i>=8 && i<10){
+      if (i >= 8 && i < 10) {
         temp += byteToString[i];
-        if(i==9){
+        if (i == 9) {
           DinnerFeedH = temp.toInt();
           temp = "";
         }
       }
-      if(i>=10 && i<12){
+      if (i >= 10 && i < 12) {
         temp += byteToString[i];
-        if(i==11){
+        if (i == 11) {
           DinnerFeedM = temp.toInt();
           temp = "";
         }
       }
     }
     byteToString = "";
-    secommandActive = false;
+    timeSetActive = false;
   }
 
-  if((t.hour == MorningFeedH && t.min == MorningFeedM) ||(t.hour == LunchFeedH && t.min == LunchFeedM) ||(t.hour == DinnerFeedH && t.min == DinnerFeedM)){
-    if(t.sec == 0){
+  if ((t.hour == MorningFeedH && t.min == MorningFeedM) || (t.hour == LunchFeedH && t.min == LunchFeedM) || (t.hour == DinnerFeedH && t.min == DinnerFeedM)) {
+    if (t.sec == 0) {
       feeding();
     }
   }
 
-  if(remainCheckActive){
-    if(millis() - WhenFeedsec >= 14000){
+  if (remainCheckActive) {
+    if (millis() - WhenFeedsec >= 14000) {
       remainFeed = getWeight();
       Serial.println(remainFeed);
-      
-      if(feedCNT > 8){
-          feedData[9] = remainFeed;
-          feedData[0] = feedData[1];
-          for(int i = 1; i < 9; i++){
-              feedData[i] = feedData[i+1];
-              delay(100);
-            }
+
+      if (feedCNT > 8) {
+        feedData[9] = remainFeed;
+        feedData[0] = feedData[1];
+        for (int i = 1; i < 9; i++) {
+          feedData[i] = feedData[i + 1];
+          delay(100);
         }
-      else if(feedCNT <= 8){
-          feedData[(feedCNT % 10)] = remainFeed;
-        }
+      }
+      else if (feedCNT <= 8) {
+        feedData[feedCNT] = remainFeed;
+      }
       feedCNT += 1;
       remainCheckActive = false;
     }
   }
-  
+
   delay(100);
 }
 
-void mp3(){
+
+void mp3() {
   //int song = random(1, 3);
   myDFPlayer.play(1);
 }
-float getWeight(){
+float getWeight() {
+  // zero point of my loadcell censor
+  // 5kg loadcell, in grams
   return (scale.get_units() * 0.453592 * 0.35 * 100) * -1;
 }
 
-void feeding(){
+void feeding() {
   mp3();
   tempWeight = 0;
-  while(tempWeight < feedAmount){
-    for(int i=0; i<32; i++) {  // 64 * 32 = 2048 한바퀴
+  while (tempWeight < feedAmount) {
+    for (int i = 0; i < 32; i++) { // 64 * 32 = 2048 한바퀴
       myStepper.step(stepsPerRevolution);
       tempWeight = getWeight();
-      if(tempWeight > feedAmount) break;
+      if (tempWeight > feedAmount) break;
     }
   }
   delay(500);
